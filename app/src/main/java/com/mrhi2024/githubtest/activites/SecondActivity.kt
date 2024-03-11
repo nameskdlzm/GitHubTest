b6999471fe04890bb790846e63329d0d7651de60
nameskdlzm <nameskdlzm@gmail.com>
2024-03-11 13:42:42 +0900

    view binding with SecondActivity

--- app/src/main/java/com/mrhi2024/githubtest/activites/SecondActivity.kt
+++ app/src/main/java/com/mrhi2024/githubtest/activites/SecondActivity.kt
@@ -3,10 +3,15 @@ package com.mrhi2024.githubtest.activites
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mrhi2024.githubtest.R
+import com.mrhi2024.githubtest.databinding.ActivitySecond2Binding
 
 class SecondActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySecond2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second2)
+        binding = ActivitySecond2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
