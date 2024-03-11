34271c1ee967888b6a3cb0b2473d73d55a5cdba5
nameskdlzm <nameskdlzm@gmail.com>
2024-03-11 13:40:50 +0900

    add SecondActivity & Image

---
+++ app/src/main/java/com/mrhi2024/githubtest/activites/SecondActivity.kt
@@ -0,0 +1,12 @@
+package com.mrhi2024.githubtest.activites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mrhi2024.githubtest.R
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second2)
+    }
+}
