bdfb6b8d50d607c9a9818dd320ac54065d4cdcfe
nameskdlzm <nameskdlzm@gmail.com>
2024-03-11 14:04:47 +0900

    add clickListener

--- app/src/main/java/com/mrhi2024/githubtest/activites/MainActivity.kt
+++ app/src/main/java/com/mrhi2024/githubtest/activites/MainActivity.kt
@@ -2,6 +2,8 @@ package com.mrhi2024.githubtest.activites
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import com.mrhi2024.githubtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         var a=10
+        binding.tv.setOnClickListener { Toast.makeText(this, "asd", Toast.LENGTH_SHORT).show() }
 
     }
 }
